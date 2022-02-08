fa8ebd2bbf5523ce329de21cc107af8e51000320
Maksym <maxim.dybarskyy@gmail.com>
2022-02-08 22:11:06 +0100

    Implement 3 buttons observing

--- firmware/src/main/main.ino
+++ firmware/src/main/main.ino
@@ -6,50 +6,63 @@
 #include <Keyboard.h>
 
 #define PORT_POTENTIOMETER 10   // analog
-#define PORT_BUTTON 7           // digital
+#define PORT_BUTTON_A      7    // digital
+#define PORT_BUTTON_B      6    // digital
+#define PORT_BUTTON_C      5    // digital
 
 #define LIMIT 50
 
 // Keyboard
 void mute();
-void quit();
 void raise();
+void camera();
 
 // Volume
 void volume_up();
 void volume_down();
 
 // Controllers
-void read_button();
 void read_rotary();
+void read_button(int, void (*fun)());
 
 // Main
 void setup() {
-	pinMode(PORT_BUTTON, INPUT);
+	pinMode(PORT_BUTTON_A, INPUT);
+	pinMode(PORT_BUTTON_B, INPUT);
+	pinMode(PORT_BUTTON_C, INPUT);
 	Keyboard.begin();
 }
 
 void loop() {
-	read_button();
 	read_rotary();
+	read_button(0, camera);
+	read_button(1, mute);
+	read_button(2, raise);
 }
 
 // Implementations
 
-int is_high = 1;
-void read_button() {
-	int state = digitalRead(PORT_BUTTON);
+int ports[3] = { 
+	PORT_BUTTON_A, 
+	PORT_BUTTON_B, 
+	PORT_BUTTON_C
+};
+
+int states[3] = {1, 1, 1};
+
+void read_button(int number, void (*fun)()) {
+	int is_high = states[number];
+	int port = ports[number];
+	int state = digitalRead(port);
 	if (is_high == 1 && state == LOW) {
-		Keyboard.print("pressed\n");
-	}
-	if (is_high == 0 && state == HIGH) {
-		Keyboard.print("released\n");
+		fun();
 	}
 	if (state == HIGH) {
 		is_high = 1;
 	} else {
 		is_high = 0;
 	}
+	states[number] = is_high;
 }
 
 int current = 0;
@@ -83,8 +96,8 @@ void raise() {
 //	Keyboard.releaseAll();
 }
 
-void quit() {
-	Keyboard.print("quit\n");
+void camera() {
+	Keyboard.print("camera\n");
 //	Keyboard.press(KEY_LEFT_CTRL);
 //	Keyboard.press('W');
 //	Keyboard.releaseAll();
